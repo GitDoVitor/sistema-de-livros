4eae1f02881eeb6e06e29467c674caa99f928686
vitor <joaovitorsantoscs@gmail.com>
2021-03-17 22:10:02 -0300

    adicionado a classe Genero e seus componentes basicos

---
+++ src/main/java/com/bts/booksys/Genero/Genero.java
@@ -0,0 +1,16 @@
+package com.bts.booksys.Genero;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Genero {
+    @Id
+    private Long id;
+    private String nome;
+}
