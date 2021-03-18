27d15eeb907b9f0fdbb8835070452e8199a7234f
vitor <joaovitorsantoscs@gmail.com>
2021-03-17 22:10:34 -0300

    adicionado a esteriotipagem do repositorio

--- src/main/java/com/bts/booksys/Editora/EditoraRepository.java
+++ src/main/java/com/bts/booksys/Editora/EditoraRepository.java
@@ -1,6 +1,8 @@
 package com.bts.booksys.Editora;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EditoraRepository extends JpaRepository<Editora, Long> {
 }
