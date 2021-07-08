cc0d9f3cc7587073a960b393bcecfe0970a6e206
gitdovitor <joaovitorsantoscs@gmail.com>
2021-07-08 11:17:29 -0300

    Arrumado Cors e arrumado algumas requisições

--- src/main/java/com/bts/booksys/controllers/EmprestimoController.java
+++ src/main/java/com/bts/booksys/controllers/EmprestimoController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@CrossOrigin
 @RequestMapping("/emprestimos")
 public class EmprestimoController {
 
@@ -24,60 +24,85 @@ public class EmprestimoController {
         this.emprestimoService = emprestimoService;
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+
     @PostMapping("/reserva")
     public ResponseEntity<Emprestimo> reservaEmprestimo(@RequestBody Emprestimo emprestimo) throws Exception{
         emprestimoService.reservaEmprestimo(emprestimo);
         return new ResponseEntity<>(emprestimo, null, HttpStatus.CREATED);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+
     @GetMapping
     public List<Emprestimo> listaEmprestimos() {
         return emprestimoService.listaEmprestimos();
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+
     @GetMapping("/{id}")
     public Emprestimo listaEmprestimoPorId(@PathVariable Long id) {
         return emprestimoService.listaEmprestimoPorId(id);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+
     @GetMapping("/livro/{nome}")
     public List<Emprestimo> listaEmprestimosPorLivro(@PathVariable String nome) {
         return emprestimoService.listaEmprestimosPorLivro(nome);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/reservados")
+    public List<Emprestimo> listaReservados() {
+        return emprestimoService.listaEmprestimosReservados();
+    }
+
+    @GetMapping("/realizados")
+    public List<Emprestimo> listaRealizados() {
+        return emprestimoService.listaEmprestimosRealizados();
+    }
+
+    @GetMapping("/cancelados")
+    public List<Emprestimo> listaCancelados() {
+        return emprestimoService.listaEmprestimosCancelados();
+    }
+
+    @GetMapping("/andamento")
+    public List<Emprestimo> listaAndamento() {
+        return emprestimoService.listaEmprestimosAndamento();
+    }
+
+
     @DeleteMapping("/{id}")
     public void deletaEmprestimoPorId(@PathVariable Long id) {
         emprestimoService.deletaEmprestimoPorId(id);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+
     @PutMapping
     public ResponseEntity<?> editaEmprestimo(@RequestBody Emprestimo emprestimo) {
         emprestimoService.editaEmprestimo(emprestimo);
         return new ResponseEntity<>(emprestimo, null, HttpStatus.ACCEPTED);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+
     @PutMapping("/cancelar/{id}")
     public void cancelaEmprestimoPorId(@PathVariable Long id) throws Exception {
         emprestimoService.cancelaEmprestimoPorId(id);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin
     @PutMapping("/iniciar/{id}")
     public void iniciaEmprestimoPorId(@PathVariable Long id) throws Exception {
         emprestimoService.iniciaEmprestimoPorId(id);
     }
 
-    @CrossOrigin(origins = "http://localhost:3000")
+
     @PutMapping("/renovar/{id}/{dataFinal}")
     public void renovaEmprestimoPorId(@PathVariable Long id, @PathVariable(value = "dataFinal")
     @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataFinal) throws Exception {
         emprestimoService.renovaEmprestimoPorId(id, dataFinal);
+    }
+
+    @PutMapping("/finaliza/{id}")
+    public void finalizaEmprestimoPorId(@PathVariable Long id) {
+        emprestimoService.finalizaEmprestimoPorId(id);
     }
 }
